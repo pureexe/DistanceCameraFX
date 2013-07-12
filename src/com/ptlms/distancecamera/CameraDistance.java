2da84ef305c99073c6260d87a0836c4ca327f315
pakkapon phongtawee <pure1997@windowslive.com>
2013-07-12 19:04:17 +0700

    CameraDistance - Fix Comment
    Change Comment from thai to english because can't display thai comment
    in github.com

--- src/com/ptlms/distancecamera/CameraDistance.java
+++ src/com/ptlms/distancecamera/CameraDistance.java
@@ -5,36 +5,40 @@ import android.util.FloatMath;
 public class CameraDistance {
 	public float getDistance(float High,float XGravity,float DefaultGravity)
 	{
-		// คำนวณหาจากตำแหน่งเครื่องถึงวัตถุโดยใช้ ตรีโกณมิติ
-		// High คือระยะทางจากบริเวณตัวเครื่องถึงพื้น รับเข้ามาได้ทาง การกรอกของผู้ใช้ หรือผ่าน pressure sensor
-		// XGravity คือ ค่าของ accelerometerในแกน x เพื่ออ่านความเร่งเนื่องจากแรงโน้มถ่วง
-		// DefaultGravity คือค่าความเร่งเนื่องจากแรงโน้มถ่วง อย่างที่เรารู้กันว่า ทั่วไป ค่า g เฉลี่ย คือ 9.86 แต่ค่าเฉลี่ยประเทศไทยกลับเป็น 9.875 m/s2
+		 /* Calculate the distance from device to object using trigonometry.
+			Variable :
+			High -> the distance from the device to the ground. get from user input or pressure sensor.
+			XGravity -> the value of x-axis accelerometer to read the acceleration to calculate angle.
+			DefaultGravity -> value of gravity acceleration. As we know that the general average g value is 9.86, but the average gravity in Thailand is 9.875 m/s2.
+		 */
 		return (float) High*((XGravity/DefaultGravity)/FloatMath.sqrt((float) (1-Math.pow((XGravity/DefaultGravity),2))));	
 	}
 	public float getHigh(float distance1,float distance2,float High,float XGravity)
 	{
-		// คำนวณหาความสูงของวัตถุโดยใช้สามเหลี่ยมคล้าย
-		// distance1 และ distance2 คือระยะทางที่ได้มาจากเมทธอด getDistance()
-		// High คือระยะทางจากบริเวณตัวเครื่องถึงพื้น รับเข้ามาได้ทาง การกรอกของผู้ใช้ หรือผ่าน pressure sensor
-		// XGravity คือ ค่าของ accelerometerในแกน x เพื่ออ่านความเร่งเนื่องจากแรงโน้มถ่วง (ในที่นี้ใช้ตรวจสอบว่าตัวเครื่อง ก้มหรือเงย หากเงย ค่าจะติดลบ)
+		/* Calculate object high use Triangle.
+		   Variable :
+		   distance1 , distance2 -> distance from getDistance()
+		   High -> the distance from the device to the ground. get from user input or pressure sensor.
+		   XGravity -> the value of x-axis accelerometer to read the acceleration to calculate angle.
+		*/
 		
-		// สลับค่า ให้ distance1 เป็นตัวมากเพื่อจะได้ไม่มีปัญหาเวลาลบ
+		// swap distance1,distance2
 		if(distance1<distance2)
 		{
 			float swaptmp = distance1;
 			distance1 = distance2;
 			distance2 = swaptmp;
 		}
-		// ตรวจสอบว่าก้มหรือเงย
+		// detect device state (angle from ground is less than 90 degree return true if more than return false)
 		if(XGravity>=0)
-			return (float)((distance1-distance2)/distance1)*High; // ถ้าก้ม
+			return (float)((distance1-distance2)/distance1)*High; // less than 90 degree
 		else
-			return (float)((distance1+distance2)/distance1)*High; // ถ้าเงย
+			return (float)((distance1+distance2)/distance1)*High; // more than 90 degree
 	}
 	
 	public float getWide(float distance1,float distance2)
 	{
-		return (float) Math.sqrt(Math.abs((distance1*distance1)-(distance2*distance2))); // ปิทาโกรัส
+		return (float) Math.sqrt(Math.abs((distance1*distance1)-(distance2*distance2))); // use Pythagoras.
 	}
 	
 }
